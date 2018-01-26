36e808c40db2b31f78b2e7ad38569934f3eacbac
vincentmuya <vincentmuya13@gmail.com>
2018-01-26 07:49:32 +0300

    created the index page

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import flask
+
+#initializing application
+
+app =Flask(__name__)
+from app import views
---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+
+#views
+@app.route('/')
+def index():
+
+    '''
+    Views root page function that returns the index page and its data
+    '''
+
+    return render_template
