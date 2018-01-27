a531a74aff63f04e250f6e95cfe1918e25eee76a
vincentmuya <vincentmuya13@gmail.com>
2018-01-27 22:08:12 +0300

    fixed the issues with tech category

--- app/views.py
+++ app/views.py
@@ -16,11 +16,11 @@ def index():
     health = get_news_source('ca','health')
     entertainment = get_news_source('ca','entertainment')
     business = get_news_source('ca','business')
-    technology_source = get_news_source('ca','science')
+    technology = get_news_source('ca','science')
     print(sport)
 
     title = "Welcome to News HighLight"
-    return render_template('index.html', title = title, technology_source = technology_source, sport=sport,science=science,health=health,business=business,entertainment=entertainment)
+    return render_template('index.html', title = title, technology = technology, sport=sport,science=science,health=health,business=business,entertainment=entertainment)
 
 @app.route('/news/<int:totalResults>')
 def news(totalResults):
