4b98fb81308358c7acd0a6493267ccf500a620bd
vincentmuya <vincentmuya13@gmail.com>
2018-01-29 10:22:21 +0300

    modified request.py

--- app/request.py
+++ app/request.py
@@ -1,14 +1,18 @@
 from app import app
 import urllib.request
 import json
-from .models import news
+from .models import News
 News = news.News
 
-#Getting api key
-api_key = app.config['NEWS_API_KEY']
+# Getting api key
+api_key = None
+# Getting the movie base url
+base_url = None
 
-#Getting the news base url
-base_url = app.config["NEWS_API_BASE_URL"]
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['MOVIE_API_KEY']
+    base_url = app.config['MOVIE_API_BASE_URL']
 
 def get_news_source(country,category):
     '''
