7f299826fc67ec80ec68ca8caa44b35a886199d8
vincentmuya <vincentmuya13@gmail.com>
2018-01-29 10:16:47 +0300

    created models.py and implemented code

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class News:
+    '''
+    News object to define News object
+    '''
+    def __init__(self,source,author,title,description,url,urlToImage,publishedAt):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
+class Sources:
